fac23f9ddafb28d73cb8483aa8486e6ca2bbb63b
KeyboardCowboy <chris.albrecht@lullabot.com>
2024-02-14 22:26:28 -0700

    Update fonts and use consistent styles.

--- src/components/seo.js
+++ src/components/seo.js
@@ -44,6 +44,9 @@ const Seo = ({ description, title, children }) => {
       <meta name="twitter:description" content={metaDescription} />
       <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-192x192.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Forum&family=Shadows+Into+Light&display=swap" rel="stylesheet" />
       {children}
     </>
   )
